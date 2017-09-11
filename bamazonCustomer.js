5b4408c7e0131e9fd3740b51a245c2c698783487
Squareof3 <haruharatakun@gmail.com>
2017-09-11 13:38:50 -0400

    Added bamazonCustomer.js

---
+++ bamazonCustomer.js
@@ -0,0 +1,71 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection (
+{
+  host: "localhost",
+  port: 3306,
+
+  user: "root",
+  password: "",
+
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    tableStuff();
+});
+
+var tableStuff = function() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        for(var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " || " + res[i].product_name + " || "
+             + res[i].department_name + " || " + res[i].price + " || " 
+             + res[i].stock_quantity);
+        }
+      questPrompt(res);
+    });
+}
+
+var questPrompt = function(res) {
+    inquirer.prompt ([{
+        type: "input",
+        name: "purchase",
+        message: "What product would you like to purchase?"
+    }]).then(function(answer) {
+        var correct = false;
+        for(var i = 0; i < res.length; i++) {
+            if(res[i].product_name === answer.purchase) {
+                correct === true;
+                var product = answer.purchase;
+                var id = i;
+
+            inquirer.prompt ({
+                type: "input",
+                name: "quantity",
+                message: "How many would you like?",
+                validate: function(value){
+                    if(isNaN(value) === false) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }).then(function(answer) {
+                if((res[id].stock_quantity-answer.quantity) > 0) {
+                    connection.query("UPDATE products SET stock_quantity = '" + (res[id].stock_quantity-answer.quantity) + 
+                    "WHERE product_name =" + product + "", 
+                    function(err, res2){
+                        console.log("Product Purchased!");
+                        tableStuff();
+                    })
+               } else {
+                   console.log("Selection invalid!");
+                   questPrompt(res);
+               }
+            });
+        }
+      }
+  });
+}
